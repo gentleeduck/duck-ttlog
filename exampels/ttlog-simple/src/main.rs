aa6c3e5bbeff624687885ef158b7938693ce0ff4
wildduck2 <wezonaser50@gmail.com>
2025-08-14 09:16:36 +0300

    feat(examples): add simple ttlog example and CLI font
    - Added new `ttlog-simple` example project with basic main.rs
    - Updated ttlog-view Cargo.toml and Cargo.lock
    - Added ANSI Shadow FIGlet font to ttlog-view/fonts for ASCII art banners

---
+++ exampels/ttlog-simple/src/main.rs
@@ -0,0 +1,20 @@
+use tracing::info;
+use ttlog::panic_hook::PanicHook;
+use ttlog::trace::Trace;
+
+fn main() {
+  let trace = Trace::init(5);
+  let buffer = trace.get_buffer();
+
+  PanicHook::install(buffer.clone());
+
+  info!("First log message");
+  info!("Second log message");
+  info!("Third log message");
+
+  // TEST: use this if you want to flush manually without panic or the hook installation
+  // Trace::flush_snapshot(buffer.clone(), "main");
+
+  // Trigger panic
+  panic!("This is a test panic!");
+}
