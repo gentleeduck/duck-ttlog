44a91b87312b640a58f7347b59d953db23f34dca
wildduck2 <wezonaser50@gmail.com>
2025-09-01 14:06:15 +0300

    ignore

--- examples/ttlog-simple/src/example_simple.rs
+++ examples/ttlog-simple/src/example_simple.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{ops::Deref, sync::Arc};
 
 use ttlog::{
   file_listener::FileListener,
@@ -32,36 +32,29 @@ pub fn example_simple() -> Result<(), Box<dyn std::error::Error>> {
   Ok(())
 }
 
-// use std::sync::Arc;
-//
-// use ttlog::{
-//   file_listener::FileListener,
-//   trace::Trace,
-//   ttlog_macros::{error, info, warn},
-// };
-//
-// pub fn example_simple() -> Result<(), Box<dyn std::error::Error>> {
-//   println!("TTLog Quick Start Example");
-//
-//   // Step 1: Initialize the tracing system
-//   let trace = Trace::init(4096, 64, "default", Some("./tmp/"));
-//   trace.add_listener(Arc::new(FileListener::new("./tmp/ttlog.log")?));
-//   trace.add_listener(Arc::new(ttlog::stdout_listener::StdoutListener::new()));
-//
-//   // Step 2: Use standard tracing macros to log
-//   info!("Application started successfully");
-//   warn!("Something might be wrong in the session handler");
-//   error!("An error occurred in the DB it might be shutting down");
-//
-//   // Step 3: Log with structured data
-//   let user_id = 42;
-//   let username = "alice";
-//   info!(user_id = user_id, username = username, "User logged in");
-//
-//   panic!("SIGINT received, shutting down!!");
-//
-//   println!("Done! Check ./tmp/ for ttlog-*.bin files");
-//   println!("Run: ls -la ./tmp/ttlog-*.bin");
-//
-//   Ok(())
-// }
+fn foo() {
+  let y = DuckBox::new(String::from("hello"));
+  with_string(&y);
+
+  println!("{:?} {:?}", &x, z);
+}
+
+fn with_string(x: &str) -> &str {
+  x
+}
+
+#[derive(Debug)]
+struct DuckBox<T>(T);
+
+impl<T> DuckBox<T> {
+  pub fn new(v: T) -> Self {
+    Self(v)
+  }
+}
+
+impl<T> Deref for DuckBox<T> {
+  type Target = T;
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
